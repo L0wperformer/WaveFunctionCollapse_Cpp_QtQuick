3cb76a1427bd07645ac7d3b651c23d5729934e50
Rosenfeld, Pascal <rosenfeld.pascal1@gmail.com>
2022-09-18 21:12:50 +0200

    Added class for Tile

---
+++ tile.h
@@ -0,0 +1,16 @@
+#ifndef TILE_H
+#define TILE_H
+
+#include <QObject>
+
+class Tile : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Tile(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // TILE_H
